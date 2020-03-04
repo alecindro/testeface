b2dccfce0fa473541516b78799e38c967111a220
Alecindro Steinke Castilho <alecindro.castilho@orsegups.com.br>
2020-03-04 17:10:34 -0300

    initial commit

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.com.alecindro.testeface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
